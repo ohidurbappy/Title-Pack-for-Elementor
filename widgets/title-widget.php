9de9d06bdf5637483ce79bad728f0ab86619cb9d
Ohidur Rahman Bappy <22405409+ohidurbappy@users.noreply.github.com>
2024-10-16 05:32:22 +0000

    updated

--- widgets/title-widget.php
+++ widgets/title-widget.php
@@ -68,11 +68,89 @@ class Elementor_Title_Widget extends \Elementor\Widget_Base {
 			[
 				'label' => esc_html__( 'Text Color', 'elementor-addon' ),
 				'type' => \Elementor\Controls_Manager::COLOR,
+				'default' => '#ffffff',
 				'selectors' => [
-					'{{WRAPPER}} .tp-styled-title' => 'color: {{VALUE}};',
+					'{{WRAPPER}} .title-widget-s1' => 'color: {{VALUE}};',
 				],
 			]
 		);
+
+		// add background color control
+		$this->add_control(
+			'title_background_color',
+			[
+				'label' => esc_html__( 'Background Color', 'elementor-addon' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'default' => '#e80028',
+				'selectors' => [
+					'{{WRAPPER}} .title-widget-s1' => 'background-color: {{VALUE}};',
+					'{{WRAPPER}} .title-widget-s1:after' => 'border-top-color: {{VALUE}};',
+				],
+			]
+		);
+
+		// font size control
+		$this->add_control(
+			'title_font_size',
+			[
+				'label' => esc_html__( 'Font Size', 'elementor-addon' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px', 'em', 'rem' ],
+				'range' => [
+					'px' => [
+						'min' => 10,
+						'max' => 100,
+					],
+				],
+				'default' => [
+					'size' => 16,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .title-widget-s1' => 'font-size: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
+
+		// font weight control
+		$this->add_control(
+			'title_font_weight',
+			[
+				'label' => esc_html__( 'Font Weight', 'elementor-addon' ),
+				'type' => \Elementor\Controls_Manager::SELECT,
+				'options' => [
+					'normal' => esc_html__( 'Normal', 'elementor-addon' ),
+					'bold' => esc_html__( 'Bold', 'elementor-addon' ),
+				],
+				'default' => 'bold',
+				'selectors' => [
+					'{{WRAPPER}} .title-widget-s1' => 'font-weight: {{VALUE}};',
+				],
+			]
+		);
+
+		// html wrapper control
+		$this->add_control(
+			'title_html_tag',
+			[
+				'label' => esc_html__( 'HTML Tag', 'elementor-addon' ),
+				'type' => \Elementor\Controls_Manager::SELECT,
+				'options' => [
+					'h1' => esc_html__( 'H1', 'elementor-addon' ),
+					'h2' => esc_html__( 'H2', 'elementor-addon' ),
+					'h3' => esc_html__( 'H3', 'elementor-addon' ),
+					'h4' => esc_html__( 'H4', 'elementor-addon' ),
+					'h5' => esc_html__( 'H5', 'elementor-addon' ),
+					'h6' => esc_html__( 'H6', 'elementor-addon' ),
+					'p' => esc_html__( 'P', 'elementor-addon' ),
+					'div' => esc_html__( 'DIV', 'elementor-addon' ),
+				],
+				'default' => 'h2',
+				// 'selectors' => [
+				// 	'{{WRAPPER}} .title-widget-s1' => 'display: inline-block;',
+				// ],
+			]
+		);
+
 
 		$this->end_controls_section();
 
@@ -87,9 +165,12 @@ class Elementor_Title_Widget extends \Elementor\Widget_Base {
 			return;
 		}
 		?>
-		<p class="tp-styled-title title-widget-s1">
+		
+
+
+		<<?php echo $settings['title_html_tag']; ?> class="title-widget-s1">
 			<?php echo $settings['title']; ?>
-		</p>
+		</<?php echo $settings['title_html_tag']; ?>>
 		<?php
 	}
 
@@ -100,9 +181,9 @@ class Elementor_Title_Widget extends \Elementor\Widget_Base {
 			return;
 		}
 		#>
-		<p class="tp-styled-title title-widget-s1">
+		<{{ settings.title_html_tag }} class="title-widget-s1">
 			{{ settings.title }}
-		</p>
+		</{{ settings.title_html_tag }}>
 		<?php
 	}
 }

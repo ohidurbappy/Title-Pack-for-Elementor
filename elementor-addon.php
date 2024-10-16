a42cef39e4cd15a4bdb864a77fe8966d8c3be9d5
Ohidur Rahman Bappy <22405409+ohidurbappy@users.noreply.github.com>
2024-10-16 05:02:34 +0000

    Add title widget CSS styles and dependencies

--- elementor-addon.php
+++ elementor-addon.php
@@ -20,3 +20,23 @@ function register_title_pack_widgets( $widgets_manager ) {
 
 }
 add_action( 'elementor/widgets/register', 'register_title_pack_widgets' );
+
+
+
+/**
+ * Register scripts and styles for Elementor title pack widgets.
+ */
+function elementor_title_pack_widgets_dependencies() {
+
+	/* Scripts */
+	// wp_register_script( 'widget-script-1', plugins_url( 'assets/js/widget-script-1.js', __FILE__ ) );
+	// wp_register_script( 'widget-script-2', plugins_url( 'assets/js/widget-script-2.js', __FILE__ ), [ 'external-library' ] );
+	// wp_register_script( 'external-library', plugins_url( 'assets/js/libs/external-library.js', __FILE__ ) );
+
+	/* Styles */
+	wp_register_style( 'title-widget', plugins_url( 'assets/css/title-widget.css', __FILE__ ) );
+	// wp_register_style( 'widget-style-2', plugins_url( 'assets/css/widget-style-2.css', __FILE__ ), [ 'external-framework' ] );
+	// wp_register_style( 'external-framework', plugins_url( 'assets/css/libs/external-framework.css', __FILE__ ) );
+
+}
+add_action( 'wp_enqueue_scripts', 'elementor_title_pack_widgets_dependencies' );
--- widgets/title-widget.php
+++ widgets/title-widget.php
@@ -1,6 +1,11 @@
 <?php
 class Elementor_Title_Widget extends \Elementor\Widget_Base {
 
+	public function get_style_depends() {
+		return [ 'style-handle' ];
+	}
+	
+
 	public function get_name() {
 		return 'title_widget';
 	}
@@ -10,7 +15,7 @@ class Elementor_Title_Widget extends \Elementor\Widget_Base {
 	}
 
 	public function get_icon() {
-		return 'eicon-code';
+		return 'eicon-editor-h1';
 	}
 
 	public function get_categories() {
@@ -21,15 +26,83 @@ class Elementor_Title_Widget extends \Elementor\Widget_Base {
 		return [ 'title', 'style' ];
 	}
 
+	public function register_controls(){
+
+		// Content Tab Start
+
+		$this->start_controls_section(
+			'section_title',
+			[
+				'label' => esc_html__( 'Title', 'elementor-addon' ),
+				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+		$this->add_control(
+			'title',
+			[
+				'label' => esc_html__( 'Title', 'elementor-addon' ),
+				'type' => \Elementor\Controls_Manager::TEXTAREA,
+				'default' => esc_html__( 'Styled Title 1', 'elementor-addon' ),
+			]
+		);
+
+		$this->end_controls_section();
+
+		// Content Tab End
+
+
+
+		// Style Tab Start
+
+		$this->start_controls_section(
+			'section_title_style',
+			[
+				'label' => esc_html__( 'Title', 'elementor-addon' ),
+				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_control(
+			'title_color',
+			[
+				'label' => esc_html__( 'Text Color', 'elementor-addon' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .tp-styled-title' => 'color: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->end_controls_section();
+
+		// Style Tab End
+
+	}
+
 	protected function render() {
+		$settings = $this->get_settings_for_display();
+
+		if ( empty( $settings['title'] ) ) {
+			return;
+		}
 		?>
-		<p> Hello World </p>
+		<p class="tp-styled-title title-widget-s1">
+			<?php echo $settings['title']; ?>
+		</p>
 		<?php
 	}
 
 	protected function content_template() {
 		?>
-		<p> Hello World </p>
+		<#
+		if ( '' === settings.title ) {
+			return;
+		}
+		#>
+		<p class="tp-styled-title title-widget-s1">
+			{{ settings.title }}
+		</p>
 		<?php
 	}
 }
